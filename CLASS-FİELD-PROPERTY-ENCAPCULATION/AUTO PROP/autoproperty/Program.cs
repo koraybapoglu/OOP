513a004eadeb4a52f238dc5a89ef68a2896a6c5c
Koray Bapoğlu <koray06_61@hotmail.com>
2023-07-15 14:47:48 +0300

    AUTO PROP

---
+++ CLASS-FİELD-PROPERTY-ENCAPCULATION/AUTO PROP/autoproperty/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autoproperty
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			üyebilgileri yeniuye = new üyebilgileri();
+			Console.WriteLine(yeniuye.ÜyeAdi);
+			Console.WriteLine(yeniuye.ÜyeSoyadi);
+			yeniuye.ÜyeAdi = "Deneme Ad";
+			yeniuye.ÜyeSoyadi = "Deneme Soyad";
+			Console.WriteLine("Deneme Üye Eklendi !");
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine(yeniuye.ÜyeAdi);
+			Console.WriteLine(yeniuye.ÜyeSoyadi);
+			Console.ReadLine();
+
+			/* EKRAN ÇIKTISI */
+			/*   Koray
+				 Bapoğlu
+				 Deneme Üye Eklendi !
+
+
+				 Deneme Ad
+				 Deneme Soyad */
+
+		}
+	}
+}
---
+++ CLASS-FİELD-PROPERTY-ENCAPCULATION/AUTO PROP/autoproperty/üyebilgileri.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autoproperty
+{
+	public class üyebilgileri
+	{
+        public string ÜyeAdi { get; set; } = "Koray";
+        public string ÜyeSoyadi { get; set; } = "Bapoğlu";
+    }
+}
