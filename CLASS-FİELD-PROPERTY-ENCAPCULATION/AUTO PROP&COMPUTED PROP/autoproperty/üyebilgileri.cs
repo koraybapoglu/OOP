1af8fd1422fdf3a574bf1c2b7269e551e857e7e4
Koray Bapoğlu <koray06_61@hotmail.com>
2023-07-15 14:56:24 +0300

    PROPERTY

---
+++ CLASS-FİELD-PROPERTY-ENCAPCULATION/AUTO PROP&COMPUTED PROP/autoproperty/üyebilgileri.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autoproperty
+{
+	public class üyebilgileri
+	{
+		public string ÜyeAdi { get; set; } = "Koray";
+		public string ÜyeSoyadi { get; set; } = "Bapoğlu";
+		int s1 = 5;
+		int s2 = 10;
+		/* COMPUTED PROPERTY */
+		public int Topla
+		{
+			get
+			{
+				return s1 + s2;
+			}
+		}
+	}
+}
