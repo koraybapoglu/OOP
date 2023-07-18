ab5faaea1453887dc9feef93b577415570eff8fd
Koray Bapoğlu <koray06_61@hotmail.com>
2023-07-19 02:41:09 +0300

    const

---
+++ CONSTRUCTOR_METHOD/DEFAULT_CONSTRUCTOR_METHOD/CLASS/UyeBilgileri.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultConstructor
+{
+	internal class UyeBilgileri
+	{
+		//Constructor Tanımlamadım !
+		private string _UyeAdi;
+		public string UyeAdi
+		{
+			get
+			{
+				return _UyeAdi;
+			}
+			set
+			{
+				_UyeAdi = value;
+			}
+		}
+		public string UyeSoyadi { get; set; } //Field Tanımlamadım otomatik sistem tanımladı !
+		private int _UyeYasi;
+		public int UyeYasi
+		{
+			get
+			{
+				return _UyeYasi;
+			}
+			set
+			{
+				_UyeYasi = value;
+			}
+		}
+		public string UyeCinsiyet { get; set; } //Field Tanımlamadım otomatik sistem tanımladı !
+
+	}
+}
+
---
+++ CONSTRUCTOR_METHOD/DEFAULT_CONSTRUCTOR_METHOD/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace DefaultConstructor
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			UyeBilgileri uyeBilgileri = new UyeBilgileri(); //'()' kullanarak nesnenin yapıcı(constructor) methodunu çağırdım fakat constructor yok !
+			uyeBilgileri.UyeAdi = "Koray";
+			uyeBilgileri.UyeSoyadi = "Bapoğlu";
+			uyeBilgileri.UyeYasi = 22;
+			uyeBilgileri.UyeCinsiyet = "Erkek";
+			Console.WriteLine("Adı:" + uyeBilgileri.UyeAdi);
+			Console.WriteLine("Soyadı:" + uyeBilgileri.UyeSoyadi);
+			Console.WriteLine("Yaşı:" + uyeBilgileri.UyeYasi);
+			Console.WriteLine("Cinsiyeti:" + uyeBilgileri.UyeCinsiyet);
+			Console.ReadLine();
+			//-------------------------------KOD ÇIKTISI !-------------------------------------------
+			/*
+			 Adı:Koray
+             Soyadı:Bapoğlu
+             Yaşı:22
+             Cinsiyeti:Erkek
+			 */
+			//-----------------------------------------------------------------------------------------------------------------------------
+			/*
+			 Herhangi bir hata almadık çünkü c# dilinde eğer oluşturduğumuz classın herhangi bir constructor(yapıcı) methodu bulunmuyorsa
+			 kendisi -DEFAULT CONSTRUCTOR- tanımlayarak Nesneyi oluşturuyor.Bunada 'DEFAULT CONSTRUCTOR' diyoruz.
+			*/
+
+		}
+	}
+}
