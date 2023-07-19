11cc34ee922a916c3e4ce67f7b974178213802e7
Koray Bapoğlu <koray06_61@hotmail.com>
2023-07-19 03:01:39 +0300

    const

---
+++ CONSTRUCTOR_METHOD/PARAMETERS_CONSTURCTOR_METHOD/Class/UyeBilgileri.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersConstructor
+{
+	internal class UyeBilgileri
+	{
+		public UyeBilgileri(string UyeAdi, string UyeSoyadi, int UyeYasi, string UyeCinsiyet) //Parametreli Constructor Oluşturduk !
+		{
+			Console.WriteLine("Nesne Oluşturuluyor !");
+			this.UyeAdi = UyeAdi; //THİS KEYWORDU KULLANILARAK BU CLASS'TAKİ UyeAdi Propertysi çağırıldı !
+			this.UyeSoyadi = UyeSoyadi; //THİS KEYWORDU KULLANILARAK BU CLASS'TAKİ UyeSoyadi Propertysi çağırıldı !
+			this.UyeCinsiyet = UyeCinsiyet; //THİS KEYWORDU KULLANILARAK BU CLASS'TAKİ UyeCinsiyet Propertysi çağırıldı !
+			this.UyeYasi = UyeYasi; //THİS KEYWORDU KULLANILARAK BU CLASS'TAKİ UyeYasi Propertysi çağırıldı !
+		}
+		private string _UyeAdi;
+		public string UyeAdi
+		{
+			get
+			{
+				return _UyeAdi;
+			}
+			set
+			{
+				_UyeAdi = value;
+			}
+		}
+		private string _UyeSoyadi;
+		public string UyeSoyadi
+		{
+			get
+			{
+				return _UyeSoyadi;
+			}
+			set
+			{
+				_UyeSoyadi = value;
+			}
+		}
+		private int _UyeYasi;
+		public int UyeYasi
+		{
+			get
+			{
+				return _UyeYasi;
+			}
+			set
+			{
+				_UyeYasi = value;
+			}
+		}
+		string _UyeCinsiyet;
+		public string UyeCinsiyet { get; set; }
+	}
+}
---
+++ CONSTRUCTOR_METHOD/PARAMETERS_CONSTURCTOR_METHOD/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ParametersConstructor
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			UyeBilgileri Koray = new UyeBilgileri("Koray","Bapoğlu",22,"Erkek"); //PARAMETRELİ CONSTRUCTOR'A PARAMETLERİ VERİLEREK NESNE
+																				 // OLUŞTURMASI YAPILDI.
+			Console.WriteLine(Koray.UyeAdi);
+			Console.WriteLine(Koray.UyeSoyadi);
+			Console.WriteLine(Koray.UyeYasi);
+			Console.WriteLine(Koray.UyeCinsiyet);
+			Console.ReadLine();
+			//-------------------------------KOD ÇIKTISI !-------------------------------------------
+			/*
+			 Nesne Oluşturuluyor !
+             Koray
+             Bapoğlu
+             22
+             Erkek
+			 */
+		}
+	}
+}
