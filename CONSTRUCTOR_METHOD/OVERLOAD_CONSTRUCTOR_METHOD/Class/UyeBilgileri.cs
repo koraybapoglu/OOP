7f271d20e21db41f269bfc8deb85ad4e2f33a12b
Koray Bapoğlu <koray06_61@hotmail.com>
2023-07-19 03:51:03 +0300

    const

---
+++ CONSTRUCTOR_METHOD/OVERLOAD_CONSTRUCTOR_METHOD/Class/UyeBilgileri.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersConstructor
+{
+	internal class UyeBilgileri
+	{
+		public UyeBilgileri(string UyeAdi, string UyeSoyadi, int UyeYasi, string UyeCinsiyet) // Tüm Parametreli Alan Constructor Oluşturduk !
+		{
+			this.UyeAdi = UyeAdi; //THİS KEYWORDU KULLANILARAK BU CLASS'TAKİ UyeAdi Propertysi çağırıldı !
+			this.UyeSoyadi = UyeSoyadi; //THİS KEYWORDU KULLANILARAK BU CLASS'TAKİ UyeSoyadi Propertysi çağırıldı !
+			this.UyeCinsiyet = UyeCinsiyet; //THİS KEYWORDU KULLANILARAK BU CLASS'TAKİ UyeCinsiyet Propertysi çağırıldı !
+			this.UyeYasi = UyeYasi; //THİS KEYWORDU KULLANILARAK BU CLASS'TAKİ UyeYasi Propertysi çağırıldı !
+		}
+		public UyeBilgileri() // Hiçbir Parametre almayan Boş Constructor Oluşturduk!
+		{
+			this.UyeAdi = "Girilmedi !";
+			this.UyeSoyadi = "Girilmedi !";
+			this.UyeYasi = 0; ;
+			this.UyeCinsiyet = "Girilmedi !";
+		}
+		public UyeBilgileri(string UyeAdi) //Üye Adı parametresini alan Constructor Oluşturduk !
+		{
+			this.UyeAdi = UyeAdi;
+			this.UyeSoyadi = "Girilmedi !";
+			this.UyeYasi = 0; ;
+			this.UyeCinsiyet = "Girilmedi !";
+		}
+		public UyeBilgileri(string UyeAdi, string UyeSoyadi) //Üyenin Ad ve Soyad parametresini alan Constructor Oluşturduk !
+		{
+			this.UyeAdi = UyeAdi;
+			this.UyeSoyadi = UyeSoyadi;
+			this.UyeYasi = 0; ;
+			this.UyeCinsiyet = "Girilmedi !";
+		}
+		public UyeBilgileri(string UyeAdi, string UyeSoyadi, int UyeYasi) //Üyenin ad,soyad ve yaş parametresini alan Constructor Oluşturduk !
+		{
+			this.UyeAdi = UyeAdi;
+			this.UyeSoyadi = UyeSoyadi;
+			this.UyeYasi = UyeYasi; ;
+			this.UyeCinsiyet = "Girilmedi !";
+		}
+
+		private string _UyeAdi;
+		public string UyeAdi
+		{
+			get
+			{
+				return _UyeAdi;
+			}
+			set
+			{
+				_UyeAdi = value;
+			}
+		}
+		private string _UyeSoyadi;
+		public string UyeSoyadi
+		{
+			get
+			{
+				return _UyeSoyadi;
+			}
+			set
+			{
+				_UyeSoyadi = value;
+			}
+		}
+		private int _UyeYasi;
+		public int UyeYasi
+		{
+			get
+			{
+				return _UyeYasi;
+			}
+			set
+			{
+				_UyeYasi = value;
+			}
+		}
+		public string UyeCinsiyet { get; set; } //Otomatik Field oluşturan property tanımladık.
+	}
+}
---
+++ CONSTRUCTOR_METHOD/OVERLOAD_CONSTRUCTOR_METHOD/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Net.Http.Headers;
+
+namespace ParametersConstructor
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			UyeBilgileri bos = new UyeBilgileri();
+			UyeBilgileri bosadi = new UyeBilgileri("Koray");
+			UyeBilgileri bosadisoyadi = new UyeBilgileri("Koray", "Bapoğlu");
+			UyeBilgileri bosadisoyadiyasi = new UyeBilgileri("Koray", "Bapoğlu", 22);
+			UyeBilgileri dolu = new UyeBilgileri("Koray", "Bapoğlu", 22, "Erkek");
+			UyeBilgileri[] uyes = new UyeBilgileri[] { bos, bosadi, bosadisoyadi, bosadisoyadiyasi, dolu };
+			foreach (var item in uyes)
+			{
+				Console.WriteLine("ADI:" + item.UyeAdi);
+				Console.WriteLine("SOYADI:" + item.UyeSoyadi);
+				Console.WriteLine("YAŞI:" + item.UyeYasi);
+				Console.WriteLine("CİNSİYETİ:" + item.UyeCinsiyet);
+				Console.WriteLine("---------------------------------------------");
+				Console.WriteLine();
+			}
+			Console.ReadLine();
+			//-------------------------------KOD ÇIKTISI !-------------------------------------------
+			/*
+             ADI:Girilmedi !
+             SOYADI:Girilmedi !
+             YAŞI:0
+             CİNSİYETİ:Girilmedi !
+             ---------------------------------------------   ("HİÇBİR PARAMETRE ALMAYAN CONSTRUCTOR(YAPICI METHOD ÇALIŞTI)")
+
+             ADI:Koray
+             SOYADI:Girilmedi !
+             YAŞI:0
+             CİNSİYETİ:Girilmedi !
+             ---------------------------------------------  ("AD PARAMETRESİ ALAN CONSTRUCTOR(YAPICI METHOD ÇALIŞTI)")
+
+             ADI:Koray
+             SOYADI:Bapoğlu
+             YAŞI:0
+             CİNSİYETİ:Girilmedi !
+             --------------------------------------------- ("AD VE SOYAD PARAMETRESİ ALAN CONSTRUCTOR(YAPICI METHOD ÇALIŞTI)")
+
+             ADI:Koray
+             SOYADI:Bapoğlu
+             YAŞI:22
+             CİNSİYETİ:Girilmedi !
+             --------------------------------------------- ("AD,SOYAD,YAŞ PARAMETRESİ ALAN CONSTRUCTOR(YAPICI METHOD ÇALIŞTI)")
+
+             ADI:Koray
+             SOYADI:Bapoğlu
+             YAŞI:22
+             CİNSİYETİ:Erkek
+             --------------------------------------------- (BÜTÜN PARAMETLERİ ALAN CONSTRUCTOR(YAPICI METHOD ÇALIŞTI)")
+			 */
+		}
+	}
+}
