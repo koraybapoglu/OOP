ed9430807bb46dc027a9fe9b63a2bb919f8225be
Koray Bapoğlu <koray06_61@hotmail.com>
2023-07-25 00:22:43 +0300

    commit

---
+++ STATIC_CONSTRUCTOR_METHOD/Class/MyClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Constructor
+{
+	internal class MyClass
+	{
+		public string UyeAdi { get; set; }
+		public string UyeSoyadi { get; set; }
+		public int UyeYasi { get; set; }
+		public MyClass()
+		{
+			//BU SINIFTAN NESNE ÜRETİLİRKEN İLK TETİKLENECEK OLAN METHODDUR.
+			Console.WriteLine("Üye Oluşturuluyor.");
+		}
+		static MyClass()
+		{
+			//BU SINIFTAN İLK NESNE ÜRETİLİRKEN İLK TETİKLENECEK OLAN METHODDUR.
+			Console.WriteLine("İlk Üye Oluşturuluyor.");
+			// STATİC CONSTRUCTOR TETİKLENEBİLMESİ İÇİN İLLA Kİ STATİC CONSTRUCTOR OLUŞTURMAMIZA GEREK YOKTUR.
+			// İLGİLİ SINIF İÇİNDE HERHANGİ BİR STATİC YAPILANMANINDA TETİKLENMESİ STATİC COSNT. TETİKLENMESİNİ SAĞLAYACAKTIR.
+		}
+	}
+}
---
+++ STATIC_CONSTRUCTOR_METHOD/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Static_Constructor
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			void Getir(MyClass uye)
+			{
+				Console.WriteLine("ÜYE ADI:" + " " + uye.UyeAdi);
+				Console.WriteLine("ÜYE SOYADI:" + " " + uye.UyeSoyadi);
+				Console.WriteLine("ÜYE YAŞI:" + " " + uye.UyeYasi);
+				Console.WriteLine("*******************************************");
+			}
+			MyClass uye1 = new MyClass();
+			uye1.UyeAdi = "Koray";
+			uye1.UyeSoyadi = "Bapoğlu";
+			uye1.UyeYasi = 22;
+			Console.WriteLine("İLK Üye Oluşturuldu !");
+			Getir(uye1);
+			MyClass uye2 = new MyClass();
+			uye2.UyeAdi = "DenemeUye";
+			uye2.UyeSoyadi = "DenemeUyeSoyadi";
+			uye2.UyeYasi = 33;
+			Getir(uye2);
+			Console.WriteLine("Üye Oluşturuldu !");
+			Console.ReadLine();
+			//--------------------------------------EKRAN ÇIKTISI------------------------------------------------
+			/* İlk Üye Oluşturuluyor.
+			   Üye Oluşturuluyor.
+			   İLK Üye Oluşturuldu !
+			   ÜYE ADI: Koray
+			   ÜYE SOYADI: Bapoğlu
+			   ÜYE YAŞI: 22
+			   *******************************************
+			   Üye Oluşturuluyor.
+			   ÜYE ADI: DenemeUye
+			   ÜYE SOYADI: DenemeUyeSoyadi
+			   ÜYE YAŞI: 33
+			   *******************************************
+			   Üye Oluşturuldu !
+			   */
+		}
+	}
+}
