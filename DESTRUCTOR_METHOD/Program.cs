ac6454c6a766ff02387ee5021b9bbb9027475ed5
Koray Bapoğlu <koray06_61@hotmail.com>
2023-07-24 22:05:36 +0300

    Create Program.cs

---
+++ DESTRUCTOR_METHOD/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Destructor
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			/* BİR CLASS'TAN ÜRETİLMİŞ OLAN NESNE İMHA EDİLİRKEN OTOMATİK ÇAĞRILAN METHODDUR. */
+			x();
+			GC.Collect(); //GARBAGE COLLECTOR ÇAĞIRDIK !
+			Console.ReadLine();
+		}
+		static void x() 
+		{
+			Myclass myclass = new Myclass();
+		}
+			
+		class Myclass 
+		{
+
+            public Myclass()
+			{
+				Console.WriteLine("SA");
+			}
+			~Myclass() 
+			{
+				Console.WriteLine("İŞTE GİDİYORUM......");
+            }
+        }
+		/*
+		 * -----------------------------------EKRAN ÇIKTISI-------------------------------------------- *
+		 SA
+         İŞTE GİDİYORUM...... 
+		 */
+	}
+}
