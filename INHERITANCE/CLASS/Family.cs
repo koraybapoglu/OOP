01995d1a1f17984b25a03be4085238d0cddbc9a1
Koray Bapoğlu <koray06_61@hotmail.com>
2023-07-26 22:17:41 +0300

    inherit

---
+++ INHERITANCE/CLASS/Family.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalitimOrnek
+{
+	internal class Family
+	{
+
+
+	}
+	internal class GrandFather //FATHER CLASSI İÇİN BASE CLASSTIR.
+	{
+		public GrandFather()
+		{
+			Console.WriteLine($"{nameof(GrandFather)} Nesnesi Oluşturulmuştur.");
+		}
+		public string ElaGöz { get; set; }
+		public string Kellik { get; set; }
+		public string ŞekerHastalığı { get; set; }
+	}
+	internal class Father : GrandFather //BU CLASS'TA DELİVER CLASS(KALITIM ALAN CLASS) FATHER CLASSIDIR.BASE CLASS(KALITIM VEREN CLASS) İSE GRANDFATHER CLASSIDIR.
+	{
+		//*******************************************************************************************************************************
+		public Father()
+		{
+			Console.WriteLine($"{nameof(Father)} Nesnesi Oluşturulmuştur.");
+		}
+		public int Sakal { get; set; }
+		public int Tansiyon { get; set; }
+		//FATHER CLASSI BASE CLASSI OLAN GRANDATHER CLASSINDAN İSE ELAGÖZ ÖZELLİĞİNİ KELLİK ÖZELLİĞİNİ VE
+		//ŞEKER HASTALIĞI ÖZELLİĞİNİ KALITSAL OLARAK ALMIŞTIR.
+
+		//********************************************************************************************************************************
+	}
+	internal class Son : Father //BU CLASS'TA DELİVER CLASS(KALITIM ALAN CLASS) SON CLASSIDIR.BASE CLASS(KALITIM VEREN CLASS) İSE FATHER CLASSIDIR.
+	{ //EKSTRA OLARAK GRANDFATHER CLASSI SON CLASSI İÇİN BASE CLASS DEĞİLDİR.ATA CLASS OLARAK DEĞERLENDİREBİLİRİZ.
+		public Son()
+		{
+			Console.WriteLine($"{nameof(Son)} Nesnesi Oluşturulmuştur.");
+		}
+		public string UzunBoy { get; set; }
+		//YAPTIĞIMIZ KALITIM SONUCU SON CLASSI GRANDFATHER CLASSINDAN ELAGÖZ ÖZELLİĞİNİ KELLİK ÖZELLİĞİNİ VE
+		//ŞEKER HASTALIĞI ÖZELLİĞİNİ KALITSAL OLARAK ALMIŞTIR.
+
+		//BASE CLASSI OLAN(FATHER CLASSI GRANDFATHER CLASSI DEĞİL !!!!!!!!!!!!!!!!!) FATHER CLASSINDAN İSE SAKAL ÖZELLİĞİNİ
+		//VE TANSİYON ÖZELLİĞİNİ KALITSAL OLARAK ALMIŞTIR.
+
+		//**********************************************************************************************************************************
+	}
+}
---
+++ INHERITANCE/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace KalitimOrnek
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//NESNE OLUŞTURDUĞUMUZDA OLUŞTURMAK İSTEDİĞİMİZ NESNENİN CLASS'I EĞER KALITIM ALIYORSA İLK OLARAK KALITIM ALDIĞI CLASS'DAN
+			//NESNE OLUŞTURUR.
+
+			//EĞER O CLASS'DA BAŞKA BİR CLASS'DAN KALITIM ALIYORSA SİSTEM OTOMATİK OLARAK ONDANDA NESNE ÜRETİR.EN ÜST CLASS HANGİSİ
+			//İSE ONDAN NESNE OLUŞTURUR OTOMATİK OLARAK.
+
+			// DENEME:
+			Console.WriteLine("Son Nesne Oluşturma Başlatıldı !!!");
+			Console.WriteLine("******************************************");
+			Console.WriteLine("Çıktı:");
+			Console.WriteLine();
+			new Son(); //BURADA NE KADAR SON CLASS'INDAN NESNE OLUŞTURSAKTA SON CLASSI FATHER CLASS'DAN KALITIM ALMAKTADIR.
+			//FATHER CLASS'I İSE GRANDFATHER CLASS'INDAN KALITIM ALMAKTADIR.
+			//BURADA OLUŞTURACAĞIMIZ NESNEDE İLK OLARAK SİSTEM OTOMATİK OLARAK GRANDFATHER CLASS'INDAN NESNE OLUŞTURUR ARDINDAN FATHER NESNESİNDEN
+			//ARDINDAN İSE SON CLASS'INDAN NESNE OLUŞTURUR.
+			Console.WriteLine("******************************************");
+			Console.WriteLine("Father Nesne Oluşturma Başlatıldı !!!");
+			Console.WriteLine("******************************************");
+			Console.WriteLine("Çıktı:");
+			Console.WriteLine();
+			new Father();
+			//BURADA FATHER CLASS'INDAN YENİ BİR NESNE ÜRETMEK İSTİYORUZ.FAKAT FATHER CLASS'I GRANDFATHER CLASS'INDAN KALITIM ALMAKTADIR.
+			//BU NEDEN İLK OLARAK SİSTEM OTOMATİK OLARAK GRANDFATHER CLASS'DAN NESNE ÜRETİR ARDINDAN İSE FATHER CLASS'INDAN NESNE ÜRETİR.
+			Console.WriteLine("******************************************");
+			Console.WriteLine("GrandFather Nesne Oluşturma Başlatıldı !!!");
+			Console.WriteLine("******************************************");
+			Console.WriteLine("Çıktı:");
+			Console.WriteLine();
+			new GrandFather(); //BURA ATA CLASS'IMIZ OLDUĞU İÇİN DİREK OLARAK GRANDFATHER CLASS'INDAN YENİ BİR NESNE ÜRETİLİR.
+			Console.ReadLine();
+			/*
+			 //--------------------------------------------------------------EKRAN ÇIKTISI--------------------------------------------------
+		    Son Nesne Oluşturma Başlatıldı !!!
+            ******************************************
+            Çıktı:
+
+            GrandFather Nesnesi Oluşturulmuştur.
+            Father Nesnesi Oluşturulmuştur.
+            Son Nesnesi Oluşturulmuştur.
+            ******************************************
+            Father Nesne Oluşturma Başlatıldı !!!
+            ******************************************
+            Çıktı:
+
+            GrandFather Nesnesi Oluşturulmuştur.
+            Father Nesnesi Oluşturulmuştur.
+            ******************************************
+            GrandFather Nesne Oluşturma Başlatıldı !!!
+            ******************************************
+            Çıktı:
+
+            GrandFather Nesnesi Oluşturulmuştur.
+	         
+	         */
+		}
+	}
+}
