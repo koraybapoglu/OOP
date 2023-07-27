733248f14b916dc93ce276da238a88aba99d8a45
Koray Bapoğlu <koray06_61@hotmail.com>
2023-07-27 22:34:59 +0300

    commit

---
+++ VIRTUAL-OVERRIDE/Virtual_ve_Override_Kullanimi/Class/MyClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Virtual_Override
+{
+	internal class MyClass
+	{
+
+	}
+	class A
+	{
+       public virtual void SinirHastaligi() //BURADA TANIMLAMIŞ OLDUĞUMUZ VİRTUAL(SANAL) METHOD SAYESİNDE KALITIM ALAN KİŞİLER İSTERSE
+			                                //BUNU EZİP GEÇEBİLİR VEYA DEĞİŞTİREBİLİR.
+
+											//EĞER HİÇBİR DEĞİŞİKLİK YAPILMAZSA EZME VEYA DEĞİŞTİRME KALMAZSA KALITIM ALAN SINIF OTOMATİK
+											//OLARAK KULLANICAKTIR.
+		{
+            Console.WriteLine("Ben Sinir Hastasıyım");
+        }
+    }
+	class B:A
+	{
+		public override void SinirHastaligi() 
+		{
+            Console.WriteLine("Ben Artık Sinir Hastası Değilim"); //BURADA B SINIFI A SINIFINDAN KALITIM ALMAKTADIR.VE A SINIFINDA OLUŞTURULAN
+			//SİNİR HASTALIĞI ADLI METHODUDA OTOMATİK OLARAK ALMAKTADIR.FAKAT İSTEĞE BAĞLI OLARAK BUNU EZEBİLİR VE DEĞİŞTİREBİLİR BUNADA OVERRİDE
+			//İŞLEMİ DENİLMEKTEDİR.
+
+			//PEKİ B SINIFINDAN KALITIM ALAN C SINIFI OLUŞTURULAN SİNİR HASTALIĞI METHODU A SINIFINDA OLAN METHODU MU YOKSA EZİLMİŞ OLAN B
+			//SINIFINDAKİ METHODU MU KULLANACAKTIR ?
+        }
+	}
+	//DENEME 2 İÇİN ÜRETİLMİŞTİR.
+	//class C : B 
+	//{
+
+	//}
+	//DENEME 3 İÇİN ÜRETİLMİŞTİR.
+	class C : B //OVERRİDE İŞLEMİ YAPILMIŞ CLASS OLAN B SINIFINDAN KALITIM ALDIK.
+    {
+		public override void SinirHastaligi()
+		{
+			Console.WriteLine("Ben Artık Sinir Hastası Değilim ama Deliyim"); //NE KADAR B SINIFINDAN EZİLEN BİR METHOD OLSADA GÜN SONUNDA OVERRİDE
+			//İŞLEMİ YAPABİLİYORUZ.
+		}
+	}
+}
---
+++ VIRTUAL-OVERRIDE/Virtual_ve_Override_Kullanimi/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Virtual_Override
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//A SINIFINDAN KALITIM ALARAK VİRTUAL(SANAL) SINIFI OVERRİDE(EZEREK) DEĞİŞTİREN B SINIFI OLUŞTURULDUĞUNDA EKRANA NE ÇIKTISI OLUŞUR ?
+			#region DENEME1
+			A a = new A(); //A SINIFINDAN NESNE OLUŞTURUYORUZ.
+			a.SinirHastaligi(); //OLUŞTURULAN NESNEDEN OLUŞTURULMUŞ OLDUĞU SINIFTAN SİNİRHASTALIĞI METHODUNU ÇAĞIRAMASINI İSTİYORUZ.
+			B b = new B(); // A SINIFINDAN KALITIM ALAN B SINIFINDAN NESNE OLUŞTURUYORUZ.
+			b.SinirHastaligi(); // A SINIFINDAN BULUNAN SİNİRHASTALIĞI ADLI METHODU OVERRİDE(EZEREK) DEĞİŞTİRMİŞTİK.
+			Console.ReadLine();
+			Console.Clear();
+			//-------------------------------------------EKRAN ÇIKTISI---------------------------------------------------------------------------
+			/* 
+			 Ben Sinir Hastasıyım // A SINIFININ VİRTUAL(SANAL) OLARAK İŞARETLENEN SİNİRHASTALIĞI METHODU ÇALIŞTI.
+             Ben Artık Sinir Hastası Değilim //A SINIFINDAN KALITIM ALAN B SINIFININ OVERRİDE(EZEREK) SİNİR HASTALIĞI METHODU ÇALIŞTI.
+			//***********************************************************************************************************************************
+			//EĞER OVERRİDE ETMESEYDİK A SINIFINDA OLUŞTURMUŞ OLDUĞUMUZ METHOD İLE AYNI ŞEKİLDE ÇALIŞICAKTI.
+			 */
+			#endregion
+			//PEKİ OVERRİDE EZİLMİŞ METHODDAN KALITIM ALAN BAŞKA BİR CLASS OTOMATİK OLARAK HANGİ METHODU ÇALIŞTIRIR ?
+			#region DENEME2
+			C c=new C();
+			c.SinirHastaligi();
+			Console.ReadLine();
+			Console.Clear();
+
+			//-------------------------------------------EKRAN ÇIKTISI---------------------------------------------------------------------------
+			/*
+			 Ben Artık Sinir Hastası Değilim //B SINIFINDAN KALITIM ALAN C SINIFI OTOMATİK OLARAK EZMEDEN BU SONUCU VERDİ.
+			//*************************************************************************************************************
+			//NEDENİ İSE KALITIM ALDIĞIMIZ SINIF A DEĞİL B SINIFIDIR VE B SINIFINDA EZİLMİŞ HALDE BULUNMAKTADIR.
+			 */
+			#endregion
+			//PEKİ OVERRİDE EDİLMİŞ CLASS'TAN KALITIM ALAN SINIF KENDİNE GÖRE OVERRİDE EDEMEZ Mİ ?
+			#region DENEME3
+			c.SinirHastaligi();
+			Console.ReadLine();
+			//-------------------------------------------EKRAN ÇIKTISI---------------------------------------------------------------------------
+			// Ben Artık Sinir Hastası Değilim ama Deliyim
+
+			//EVET EDİLEBİLİYOR.ÖRNEKTEDE GÖRÜLDÜĞÜ GİBİ C SINIFINDAN OVERRİDE EDEREK METHODU EZEREK KENDİNE GÖRE DOLDURABİLİYORUZ.
+			//PEKİ NASIL ? ÇÜNKÜ A SINIFI ATA SINIFI OLDUĞU İÇİN NE KADAR B SINIFINDAN KALITIM ALSAKTA OTOMATİK OLARAK B SINIFI ÇALIŞTIRSADA
+			//GÜN SONUNDA A SINIFINDAN VİRTUAL ANAHTAR KELİMESİ İLE İŞARETLENMİŞTİR.
+			#endregion
+
+		}
+	}
+}
