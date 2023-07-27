65dec5b45c84c6a9da20ddfa20d09941fbe7fa16
Koray Bapoğlu <koray06_61@hotmail.com>
2023-07-27 23:49:21 +0300

    commit

---
+++ VIRTUAL-OVERRIDE/Virtual_Override_Mini_Project/Class/Şekil.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtual_Override_Mini_Proje
+{
+	internal class Sekil
+	{
+		protected int _Boy;
+		protected int _En;
+        public Sekil(int Boy,int En)
+        {
+			_Boy = Boy;
+			_En = En;
+        }
+		public virtual int AlanHesapla() 
+		{
+			return 0;
+		}
+    }
+    class Dikdörtgen:Sekil
+    {
+        public Dikdörtgen(int Boy,int En):base(Boy,En)
+        {
+            
+        }
+		public override int AlanHesapla()
+		{
+             return _Boy* _En;
+		}
+	}
+    class Kare : Sekil 
+    {
+        public Kare(int Boy,int En):base(Boy,En)
+        {
+               
+        }
+		public override int AlanHesapla()
+		{
+            return _Boy * _Boy;
+		}
+	}
+    class Ucgen : Sekil 
+    {
+        public Ucgen(int Boy,int En):base(Boy,En)
+        {
+               
+        }
+		public override int AlanHesapla()
+		{
+            return _Boy * _En / 2;
+		}
+	}
+    
+}
---
+++ VIRTUAL-OVERRIDE/Virtual_Override_Mini_Project/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace Virtual_Override_Mini_Proje
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Alanını Hesaplamak istediğiniz Şekli Seçiniz.\nKARE İÇİN 1\nDİKDÖRTGEN İÇİN 2\nÜÇGEN İÇİN 3 TUŞLAYINIZ.");
+			int secim = Convert.ToInt32(Console.ReadLine());
+			int boy = 0;
+			int en = 0;
+			if (secim==1) 
+			{
+                Console.WriteLine("Karenin Kenar Uzunluğunu giriniz:");
+				boy = Convert.ToInt32(Console.ReadLine());
+				en = boy;
+				Kare kare = new Kare(boy,en);
+				Console.WriteLine("KARENİN ALANI:"+kare.AlanHesapla());
+				Console.ReadLine();
+            }
+			else if (secim==2)
+			{
+				Console.WriteLine("Dikdörtgenin Kısa Kenarını Giriniz:");
+				boy=Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dikdörtgenin Uzun Kenarını Giriniz:");
+				en = Convert.ToInt32(Console.ReadLine());
+				Dikdörtgen dikdörtgen = new Dikdörtgen(boy,en);
+                Console.WriteLine("Dikdörtgenin ALANI:"+dikdörtgen.AlanHesapla());
+				Console.ReadLine();
+            }
+			else if (secim==3)
+			{
+				Console.WriteLine("Üçgenin Taban Uzunluğunu Giriniz:");
+				boy= Convert.ToInt32(Console.ReadLine());
+				Console.WriteLine("Üçgenin Yüksekliğini Giriniz:");
+				en= Convert.ToInt32(Console.ReadLine());
+				Ucgen ucgen=new Ucgen(boy,en);
+                Console.WriteLine("Üçgenin ALANI:"+ucgen.AlanHesapla());
+				Console.ReadLine();
+            }
+			else
+			{
+                Console.WriteLine("HATALI GİRİŞ YAPTINIZ LÜTFEN TEKRAR DENEYİNİZ.");
+                Console.WriteLine("PROJE KAPATILIYOR...");
+                Thread.Sleep(3000);
+				Environment.Exit(0);
+            }
+		}
+		//-------------------------------------EKRAN ÇIKTISI-----------------------------------------------------------------------------
+		/*
+		 -----------KARE-------------------------------
+		 Alanını Hesaplamak istediğiniz Şekli Seçiniz.
+         KARE İÇİN 1
+         DİKDÖRTGEN İÇİN 2
+         ÜÇGEN İÇİN 3 TUŞLAYINIZ.
+         1
+         Karenin Kenar Uzunluğunu giriniz:
+         15
+         KARENİN ALANI:225
+		----------------------------------------------
+		-----------DİKDÖRTGEN-------------------------
+		Alanını Hesaplamak istediğiniz Şekli Seçiniz.
+        KARE İÇİN 1
+        DİKDÖRTGEN İÇİN 2
+        ÜÇGEN İÇİN 3 TUŞLAYINIZ.
+        2
+        Dikdörtgenin Kısa Kenarını Giriniz:
+        20
+        Dikdörtgenin Uzun Kenarını Giriniz:
+        25
+        Dikdörtgenin ALANI:500
+		---------------------------------------------
+		---------------ÜÇGEN-------------------------
+		Alanını Hesaplamak istediğiniz Şekli Seçiniz.
+        KARE İÇİN 1
+        DİKDÖRTGEN İÇİN 2
+        ÜÇGEN İÇİN 3 TUŞLAYINIZ.
+        3
+        Üçgenin Taban Uzunluğunu Giriniz:
+        25
+        Üçgenin Yüksekliğini Giriniz:
+        30
+        Üçgenin ALANI:375
+		---------------------------------------------
+		 */
+	}
+}
