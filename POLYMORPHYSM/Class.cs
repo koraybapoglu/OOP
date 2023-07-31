d6116adc233468bd15e89879390e2dafbca53eb9
Koray Bapoğlu <koray06_61@hotmail.com>
2023-07-31 16:34:10 +0300

    polymor

---
+++ POLYMORPHYSM/Class.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorphysm
+{
+	internal class Class
+	{
+
+	}
+	class A //ATA SINIF 
+	{
+		public void X() { }
+	}
+	class B:A
+	{
+		public void Y() { }
+	}
+	class C:B
+	{
+		public void Z() { }
+	}
+
+}
---
+++ POLYMORPHYSM/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Polimorphysm
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//POLİMORPYHSM İÇİN KALITIM ŞARTTIR.
+			//BİR NESNENİN BAŞKA BİR NESNE İLE İŞARETLENEBİLMESİ İÇİN KESİNLİKLE ARADA KALITIMSAL BİR İLİŞKİ OLMASI GEREKMEKTEDİR.
+			//NESNE TABANLI PROGRAMLAMADA POLİMORPHYSM UYGULANABİLMESİ İÇİN KALITIMSAL İLİŞKİ OLMALIDIR.AKSİ MÜMKÜN DEĞİLDİR.
+			C c = new C(); //
+			c.X();         //  DOĞRU KULLANIM
+			c.Y();         //
+			c.Z();         //
+
+
+			B B = new C(); // 
+			B.X();  //    DOĞRU KULLANIM
+			B.Y(); //           
+
+			B.Z(); // =>YANLIŞ KULLANIM.
+			//NEDENİ İSE B NESNESİ B SINIFINDAN REFERANS GÖSTERİLEREK ÇALIŞMAKTADIR.
+			A A = new C();
+			A.X(); // =>DOĞRU KULLANIM
+
+			A.Y(); //
+			A.Z(); // YANLIŞ KULLANIM NEDENİ İSE A NESNESİ A SINIFINDAN REFERANS GÖSTERİLEREK ÇALIŞMAKTADIR.
+		}
+	}
+}
