8f5da1726d87f687c9c57fd7f1700cf68b609ec9
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-01 21:00:00 +0300

    commit

---
+++ THİS KEYWORD/Class/Hayvan.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedKeyword
+{
+	internal class Hayvan
+	{
+		public virtual void Tanit()
+		{
+			Console.WriteLine("Ben bir Hayvanim"); //PEKİ KALITIM OLMAYAN CLASS'TA BASE KEYWORD'Ü KULLANILAMAZ MI ?
+
+			//CEVABI HAYIR.ÇÜNKÜ BÜTÜN CLASSLAR OBJECT TÜRÜNDEN KALITIM ALDIĞI İÇİN BASE DEDİĞİMİZDE OBJECT CLASSINDA OLAN BÜTÜN METHODLARA ULAŞMIŞ OLACAĞIZ.
+		}
+
+	}
+	class Kus : Hayvan
+	{
+		public override void Tanit()
+		{
+			base.Tanit(); //BURADA BİR KALITIM OLDUĞU İÇİN BASE CLASS OLAN(HAYVAN) CLASSINDAKİ TANİT METHODUNU ÇALIŞTIRIYORUZ.
+		}
+	}
+	class Muhabbet_Kusu : Kus
+	{
+		public override void Tanit()
+		{
+			base.Tanit(); //BURADA BİR KALITIM OLDUĞU İÇİN BASE CLASS OLAN(KUS) CLASSINDAKİ TANİT METHODUNU ÇALIŞTIRIYORUZ.
+		}
+	}
+}
---
+++ THİS KEYWORD/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SealedKeyword
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Hayvan kus3 = new Hayvan();
+			Kus kus2=new Kus();
+			Muhabbet_Kusu kus = new Muhabbet_Kusu();
+			kus.Tanit();
+			kus2.Tanit();
+			kus3.Tanit();
+			Console.ReadLine();
+			/*
+			 -----------------------------------------EKRAN ÇIKTISI---------------------------------------------------------
+			 Ben bir Hayvanim
+             Ben bir Hayvanim
+             Ben bir Hayvanim
+			 */
+		}
+	}
+}
