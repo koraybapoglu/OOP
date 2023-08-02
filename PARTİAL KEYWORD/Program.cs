f0304eb66c587d968c81d2fd0524a3c9fe3b33d6
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-02 15:56:11 +0300

    commit

---
+++ PARTİAL KEYWORD/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace PartialKeyword
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//PARTİAL KEYWORD'Ü NEDİR ?
+
+			//PARTİAL KOD BÖLÜMLEMEYE YARAR.
+
+			//BÜYÜK VE KARMAŞIK YAPIDAKİ SINIFLARI DAHA OKUNABİLİR VE DÜZENLENEBİLİR PARÇALARA BÖLMEK İÇİN KULLANILABİLİR.
+
+			//İŞ BÖLÜMÜNE YARAR.
+
+			//EKİPLERİN,AYNI SINIFIN FARKLI KISIMLARINI AYNI ANDA GELİŞTİREBİLMELERİ İÇİN KULLANILABİLİR.
+
+
+			new MyClass();
+			Console.ReadLine();
+
+		}
+	}
+	partial class MyClass  //BU ŞEKİLDE MYCLASS SINIFINI PARTİAL KEYWORD'Ü KULLANARAK AYIRMIŞ OLDUK.
+	{
+        public MyClass()
+        {
+			X();
+			Y();
+        }
+		public void X() 
+		{
+            Console.WriteLine("Kişinin Bilgileri Depoya Kaydedildi !");   
+        }
+    }
+	partial class MyClass //BU ŞEKİLDE MYCLASS SINIFINI PARTİAL KEYWORD'Ü KULLANARAK AYIRMIŞ OLDUK.
+	{
+        public void Y() 
+		{
+            Console.WriteLine("Kişinin bilgileri Veritabanına Kaydedildi !");
+        }
+    }
+}
